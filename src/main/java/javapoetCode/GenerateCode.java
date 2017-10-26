6cb9d748897e6d60303a4c7eb3b384e8ecee4463
amitchauhan101 <amit.chauhan@monotype.com>
2017-10-26 11:34:08 +0530

    fixed output directory path

--- src/main/java/javapoetCode/GenerateCode.java
+++ src/main/java/javapoetCode/GenerateCode.java
@@ -6,10 +6,13 @@ import java.util.logging.Logger;
 
 public class GenerateCode {
 	private final static Logger LOGGER = Logger.getLogger(GenerateCode.class.getName());
-
+public static void main(String[] args) throws IOException {
+	GenerateCode obj = new GenerateCode();
+	obj.generateCode();
+}
 	public void generateCode() throws IOException{
 		LOGGER.log(Level.INFO, "Inside GenerateCode - Starting code generation");
-		String outputDirectory = System.getProperty("user.home").replace("\\", "/")+"/Desktop";
+		String outputDirectory = System.getProperty("user.home").replace("\\", "/")+"/Desktop/src";
 		CreateApiClass.generateApiClass(outputDirectory);
 		CreateTestScript.generateTestCase(outputDirectory);
 		LOGGER.log(Level.INFO, "Completed code generation.");
